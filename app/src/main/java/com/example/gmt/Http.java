9944046656cdd69417edbecd06aeec1c491bd0fa
yuntree <ch6871070@gmail.com>
2019-04-21 01:50:04 +0900

    [Add] 서버 통신을 위한 파일

--- app/src/main/java/com/example/gmt/Http.java
+++ app/src/main/java/com/example/gmt/Http.java
@@ -1,61 +1,75 @@
-package com.example.part9_25;
+package com.example.gmt;
 
-import android.os.AsyncTask;
-import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class HttpRequester {
-	
-	HttpTask http;
-	
-	public void request(String url, HashMap<String, String> param, HttpCallback callback) {
-		http = new HttpTask(url, param, callback);
-		http.execute();
-	}
-	
-	public void cancel() {
-		if(http != null)
-			http.cancel(true);
-	}
-	
-	private class HttpTask extends AsyncTask<Void, Void, String> {
-		String url;
-		HashMap<String, String> param;
-		HttpCallback callback;
-		
-		public HttpTask(String url, HashMap<String, String> param, HttpCallback callback) {
-			this.url = url;
-			this.param = param;
-			this.callback = callback;
-		}
-		
-		@Override
-		protected String doInBackground(Void... nothing) {
-			String response = "";
-			String postData = "";
-			PrintWriter pw = null;
-			BufferedReader in = null;
-			
-			//add~~~~~~~~~~~~~~
+public class Http {
+    public List<MovieVO> getMovies() {
 
-			return response;
-		}
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<List<MovieVO>> movies = executorService.submit(new Callable<List<MovieVO>>() {
+            @Override
+            public List<MovieVO> call() throws Exception {
+                List<MovieVO> movieVOs = null;
+                try {
+                    String strUrl = "http://172.30.1.55:8080/movies";
+                    URL url = new URL(strUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setConnectTimeout(100000);
+                    connection.setReadTimeout(100000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(false);
+                    connection.setRequestProperty("content-type", "application/json; charset=utf-8");
 
-		@Override
-		protected void onPostExecute(String result) {
-			//add~~~~~~~~~~~
-			
-		}
-	}
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == 200) {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        String jsonSource = "";
+                        while (true) {
+                            String src = bufferedReader.readLine();
+                            if (src == null) break;
+                            jsonSource += src;
+                        }
+                        JSONArray jsonArray = new JSONArray(jsonSource);
+                        movieVOs = new ArrayList<>();
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = new JSONObject(jsonArray.getString(i));
+                            MovieVO movieVO = new MovieVO(jsonObject.getLong("id"), jsonObject.getString("name"), jsonObject.getString("thumbnail"));
+                          movieVOs.add(movieVO);
+                        }
 
+                        bufferedReader.close();
+                    }
+
+                    connection.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+                return movieVOs;
+            }
+        });
+
+        try {
+            return movies.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
