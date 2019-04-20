5aa2728b5d21610ff864a6b0b8e68e25893b1c34
yuntree <ch6871070@gmail.com>
2019-04-21 01:45:07 +0900

    [Add] ViewPager 구현을 위한 어댑터 구현. MovieVO 리스트 객체를 생성하고 List의 position에 따라 알맞은 객체를 넘겨줌

--- app/src/main/java/com/example/gmt/PosterPagerAdapter.java
+++ app/src/main/java/com/example/gmt/PosterPagerAdapter.java
@@ -1,4 +1,39 @@
 package com.example.gmt;
 
-public class PosterPagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PosterPagerAdapter extends FragmentPagerAdapter {
+
+    private List<MovieVO> movieList;
+
+    public PosterPagerAdapter(FragmentManager fragmentManager, List<MovieVO> movieList) {
+        super(fragmentManager);
+//        posters = new ArrayList<>();
+//        posters.add(R.drawable.dongwon);
+//        posters.add(R.drawable.hyun);
+//        posters.add(R.drawable.jong);
+
+        if (movieList == null) {
+            this.movieList = new ArrayList<>();
+            return;
+        }
+        this.movieList = movieList;
+    }
+
+    public int getCount() {
+        return movieList.size();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        PosterFragment fragment = new PosterFragment();
+        fragment.setMovieVO(movieList.get(position));
+        return fragment;
+    }
 }
