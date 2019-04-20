7bb1abd58354f3fd5675aecc53b5530b0741a625
yuntree <ch6871070@gmail.com>
2019-04-21 01:46:51 +0900

    [Add] 서버에서 가져온 데이터들을 저장하는 객체

--- app/src/main/java/com/example/gmt/MovieVO.java
+++ app/src/main/java/com/example/gmt/MovieVO.java
@@ -1,4 +1,44 @@
 package com.example.gmt;
 
 public class MovieVO {
+
+    private Long id;
+    private String name;
+    private String thumbnail;
+
+    public MovieVO(Long id, String name, String thumbnail) {
+
+        this.id = id;
+        this.name = name;
+        this.thumbnail = thumbnail;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieVO{id : " + id + " name : " + name + " thumbnail : " + thumbnail + "}";
+    }
 }
