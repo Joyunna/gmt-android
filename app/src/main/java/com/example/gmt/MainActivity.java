adba67a72e26f29f9f5d2736ddd209b5c94fe095
yuntree <ch6871070@gmail.com>
2019-05-06 21:28:54 +0900

    [Edit] - 코드 리팩토링(메소드 분리 및 변수명 변경)

--- app/src/main/java/com/example/gmt/MainActivity.java
+++ app/src/main/java/com/example/gmt/MainActivity.java
@@ -11,97 +11,91 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.util.List;
+import com.example.gmt.adapter.PosterPagerAdapter;
+import com.example.gmt.service.PosterService;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import static com.example.gmt.utils.ToastUtil.showToast;
 
-   DrawerLayout drawer;
-  // ActionBarDrawerToggle toggle;
-   boolean isDrawerOpend;
-   Toolbar toolbar;
-   ImageView imageView;
-   ImageView imageVIew2;
-   ViewPager viewPager;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private ImageView profile;
+    private ImageView btnHamburger;
+    private ViewPager viewPager;
+    private NavigationView leftNavigationView;
+    private PosterService posterService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<MovieVO> movies = new Http().getMovies();
+        this.init();
+        this.setEventListener();
+    }
 
-        imageView = (ImageView)findViewById(R.id.my);
-        imageView.setOnClickListener(this);
+    private void init() {
+        this.drawer = findViewById(R.id.main_drawer);
+        this.profile = findViewById(R.id.main_btn_profile);
+        this.btnHamburger = findViewById(R.id.main_btn_hamburger);
+        this.viewPager = findViewById(R.id.main_poster_viewpager);
+        this.leftNavigationView = findViewById(R.id.main_left_navigation);
+        this.initToolbar();
+        this.posterService = new PosterService();
+    }
 
-        imageVIew2 = (ImageView)findViewById(R.id.ham);
-        imageVIew2.setOnClickListener(this);
-
-        ViewPager pager = (ViewPager)findViewById(R.id.poster);
-        PosterPagerAdapter pagerAdapter = new PosterPagerAdapter(getSupportFragmentManager(), movies);
-        pager.setAdapter(pagerAdapter);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("");
+    private void initToolbar() {
+        toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
-
-
-        drawer = (DrawerLayout)findViewById(R.id.main_drawer);
-        //toggle = new ActionBarDrawerToggle(this, drawer, R.string.drawer_open, R.string.drawer_close);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        toolbar.setContentInsetsAbsolute(0,0);
-       // toggle.syncState();
-
-        NavigationView naviView = (NavigationView)findViewById(R.id.main_drawer_view);
-        naviView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-                if(id == R.id.menu_drawer_home){
-
-                    showToast("home");
-                }else if(id == R.id.menu_drawer_message){
-
-                    showToast("message");
-                }else if(id == R.id.menu_drawer_add){
-
-                    showToast("add");
-                }
-                return false;
-            }
-        });
-
+        toolbar.setContentInsetsAbsolute(0, 0);
+        toolbar.setTitle("");
     }
 
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.my:
-                    drawer.openDrawer(GravityCompat.END);
-                    break;
-                case R.id.ham:
-                    drawer.openDrawer(GravityCompat.START);
-                    break;
-            }
-        }
+    private void setEventListener() {
+        drawer.setOnClickListener(this);
+        this.profile.setOnClickListener(this);
+        this.btnHamburger.setOnClickListener(this);
+        this.leftNavigationView.setNavigationItemSelectedListener(this);
+    }
 
-        /*
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-
-        if(toggle.onOptionsItemSelected(item)){
-
-            return false;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
+        posterService.refreshMovies(this);
     }
-*/
-    private void showToast(String message){
 
-        Toast t = Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        t.show();
+    public void setPosterPagerAdapter(PosterPagerAdapter pagerPosterAdapter) {
+        viewPager.setAdapter(pagerPosterAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_btn_profile:
+                drawer.openDrawer(GravityCompat.END);
+                break;
+            case R.id.main_btn_hamburger:
+                drawer.openDrawer(GravityCompat.START);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_drawer_home:
+                showToast(this, "home");
+            case R.id.menu_drawer_message:
+                showToast(this, "message");
+                return true;
+            case R.id.menu_drawer_add:
+                showToast(this, "add");
+                return true;
+            default:
+                return false;
+        }
     }
 }
